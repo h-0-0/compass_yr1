b4dde331674d589b16f10911fd5ac0453fa028e2
Henry Bourne <hwbourne@gmail.com>
2022-12-27 14:22:26 +0000

    Now using different dataset

--- labs/stattools/tests/testthat/test-binary_logistic_regression.R
+++ labs/stattools/tests/testthat/test-binary_logistic_regression.R
@@ -14,16 +14,10 @@ test_that("Binary logistic regression on simple dataset", {
 })
 
 test_that("Binary logistic regression on more complex dataset", {
-  library("mlbench")
-  data(Sonar)
-  x <- Sonar[,1:ncol(Sonar)-1]
-  y <- ifelse(Sonar[,ncol(Sonar)] == 'R', 0, 1)
-  results <- optim(par = rep(1,ncol(x)+1), fn = binlr_nll, D=x, y=y)
+  diabetes <- read.csv(url('https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv'))
+  x <- diabetes[,-ncol(diabetes)]
+  y <- diabetes[,ncol(diabetes)]
+  results <- optim(par = rep(0, ncol(x)+1), fn = binlr_nll, D=x, y=y)
   params <- signif(results$par,4)
-  expect_equal(params, c(0.7492, 1.0320, 1.0290, 1.0260, 1.0190, 1.0150, 1.0030, 0.9980, 1.0870, 0.9585, 0.9749, 1.1940,
-                         1.0140, 1.0110, 1.0090, 1.0060, 1.0010, 0.9959, 0.9947, 0.9177, 0.9829, 0.9709, 0.9684, 0.9624,
-                         0.9591, 0.9565, 0.9481, 0.9507, 0.9540, 0.9735, 0.9668, 0.9798, 0.9934, 0.9972, 0.9959, 0.9922,
-                         0.9910, 0.9984, 1.0030, 1.0050, 1.0080, 1.0070, 1.0100, 1.0130, 1.0150, 0.9814, 0.9929, 1.0070,
-                         1.0110, 1.0220, 1.0340, 1.0370, 1.0390, 1.0430, 1.0440, 1.0460, 1.0480, 1.0440, 0.9698, 1.0470,
-                         1.0500))
+  expect_equal(params, c(-0.6973, 0.1271, 0.009711, -0.03852, -0.003901,  0.0007273,  0.04765, 0.3615, -0.01526))
 })
