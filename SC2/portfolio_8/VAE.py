d11292161be82fcfee594acbac2ed0c78c42d37c
h-aze <jd18380@bp1-login01.data.bp.acrc.priv>
2023-04-22 15:22:50 +0100

    Re-trained models

--- SC2/portfolio_8/VAE.py
+++ SC2/portfolio_8/VAE.py
@@ -30,7 +30,7 @@ class Encoder(L.LightningModule):
             nn.Conv2d(2 * c_hid, 2 * c_hid, kernel_size=3, padding=1, stride=2),  # 8x8 => 4x4
             act_fn(),
             nn.Flatten(),  # Image grid to single feature vector
-            nn.Linear(2 * 16 * c_hid, latent_dim),
+            nn.Linear(2 * 16 * c_hid, latent_dim)
         )
 
     def forward(self, x):
@@ -83,7 +83,7 @@ class Decoder(L.LightningModule):
         """
         super().__init__()
         c_hid = base_channel_size
-        self.linear = nn.Sequential(nn.Linear(latent_dim, 2* 16 * c_hid), act_fn())
+        self.linear = nn.Sequential(nn.Linear(latent_dim, 2 * 16 * c_hid), act_fn())
         self.net = nn.Sequential(
             nn.ConvTranspose2d(
                 2 * c_hid, 2 * c_hid, kernel_size=3, output_padding=1, padding=1, stride=2
@@ -98,7 +98,7 @@ class Decoder(L.LightningModule):
             nn.ConvTranspose2d(
                 c_hid, num_input_channels, kernel_size=3, output_padding=1, padding=1, stride=2
             ),  # 16x16 => 32x32
-            nn.Tanh(),  # The input images is scaled between -1 and 1, hence the output has to be bounded as well
+            nn.Tanh()  # The input images is scaled between -1 and 1, hence the output has to be bounded as well
         )
 
     def forward(self, x):
